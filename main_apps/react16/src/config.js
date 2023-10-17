46fae3179d7d78b4067403b3a7f3545a66009bc7
Emil Zhai <root@derzh.com>
2023-10-17 09:47:43 +0000

    chore(react16): remove sidebar app

--- main_apps/react16/src/config.js
+++ main_apps/react16/src/config.js
@@ -6,7 +6,6 @@ const config = {
   nuxtjs2: 'http://localhost:4003',
   react16: 'http://localhost:4004',
   react17: 'http://localhost:4005',
-  sidebar: 'http://localhost:4006',
   vite: 'http://localhost:4007',
   vue2: 'http://localhost:4008',
   vue3: 'http://localhost:4009',
