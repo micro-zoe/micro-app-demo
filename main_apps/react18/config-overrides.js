51160158fae317ddf0408451a90a890ffd99cdc2
Emil Zhai <root@derzh.com>
2023-11-10 12:04:50 +0000

    fix(main/react18): remove client overlay for ignore child app errors

--- main_apps/react18/config-overrides.js
+++ main_apps/react18/config-overrides.js
@@ -1,8 +1,11 @@
+/* config-overrides.js */
 const path = require('path');
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 
-module.exports = function override(config, env) {
-  config.output.publicPath = '/main-react18/';
+module.exports = {
+  // The Webpack config to use when compiling your react app for development or production.
+  webpack: function override(config, env) {
+    config.output.publicPath = '/main-react18/';
   // config.resolve.alias = Object.assign({}, config.resolve.alias, {
   //   "@micro-zoe/micro-app/polyfill":  path.join(__dirname, '../../../micro-app/polyfill'),
   //   "@micro-zoe/micro-app":  path.join(__dirname, '../../../micro-app/lib/index.esm.js'),
@@ -16,5 +19,16 @@ module.exports = function override(config, env) {
   //   }
   //   i++
   // }
-  return config;
+    return config;
+  },
+  devServer: function(configFunction) {
+    return function(proxy, allowedHost) {
+      // Create the default config by calling configFunction with the proxy/allowedHost parameters
+      const config = configFunction(proxy, allowedHost);
+      // Change the dev server's config here...
+      // Then return your modified config.
+      config.client.overlay = false;
+      return config;
+    }
+  }
 }
