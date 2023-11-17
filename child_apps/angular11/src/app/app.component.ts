169baeaa141d741a305e15a0fe2847cec19e5705
Emil Zhai <root@derzh.com>
2023-11-17 19:02:30 +0800

    feat(child/angular11): add popstate event

--- child_apps/angular11/src/app/app.component.ts
+++ child_apps/angular11/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, NgZone } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +8,17 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   constructor(private router: Router, private ngZone: NgZone) {
+    if (window.__MICRO_APP_ENVIRONMENT__) {
+      /**
+       * 子应用跳转后向主应用发送PopStateEvent事件，使主应用响应路由变化，触发侧边栏高亮
+       * NOTE: 实际项目中并不一定需要，根据实际情况而定
+       */
+      router.events.subscribe((e) => {
+        if (e instanceof NavigationEnd) {
+          (window as any).rawWindow.dispatchEvent(new PopStateEvent('popstate', { state: null }))
+        }
+      });
+    }
     // 此处用于监听基座下发的跳转指令
     window.microApp?.addDataListener((data: Record<string, unknown>) => {
       // 当基座下发path时进行跳转
