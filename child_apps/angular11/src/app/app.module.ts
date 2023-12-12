2e46817bbf266e037f7a8bbc729045ab9696909c
Emil Zhai <root@derzh.com>
2023-12-12 09:39:36 +0000

    feat(child/angular11): add material component deps

--- child_apps/angular11/src/app/app.module.ts
+++ child_apps/angular11/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,7 +13,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
